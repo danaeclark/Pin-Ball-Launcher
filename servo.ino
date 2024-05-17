57963ccc794750c219bd953ec6ed509fd49a0435
Dana Clark <118500562+danaeclark@users.noreply.github.com>
2024-05-17 03:06:27 -0400

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -12,12 +12,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (pos = 0; pos <= 75; pos += 1) {   // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 75; pos += 1) {   // goes from 0 degrees to 75 degrees
     // in steps of 1 degree
     myservo.write(pos); 
     delay(15);
   }
-  for (pos = 75; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
+  for (pos = 75; pos >= 0; pos -= 1) {  // goes from 75 degrees to 0 degrees
     myservo.write(pos);                  // tell servo to go to position in variable 'pos'
     delay(15);                       
   }
